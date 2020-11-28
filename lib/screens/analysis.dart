b1d1dd2efbfaaee8541fe6078b672ec8b1e590b4
박석원 <rty3394@naver.com>
2020-11-28 12:41:18 +0900

    .
    .

---
+++ lib/screens/analysis.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ViewPage extends StatefulWidget {
+  ViewPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ViewPageState createState() => _ViewPageState();
+}
+
+class _ViewPageState extends State<ViewPage> {
+  int rank = 0;
+
+  @override
+  Widget ViewItem(DocumentSnapshot doc) {
+    final vid = doc.data();
+    rank++;
+    return Container(
+      child: Table(
+        children: [
+          TableRow(children: [
+            Text('판매순위:'+rank.toString()+'위',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                textAlign: TextAlign.left),
+            Padding(padding: EdgeInsets.all(0.0)),
+            Text('메뉴이름: ${vid['MenuName']}\n',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                textAlign: TextAlign.left),
+            Padding(padding: EdgeInsets.all(0.0)),
+            Text('${vid['sold']}개 판매되었습니다.',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                textAlign: TextAlign.left),
+          ])
+        ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('판매 분석'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('OrderRecord').orderBy('sold', descending: true)
+                      .snapshots()
+                  ,
+                  // ignore: missing_return
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                          children: snapshot.data.docs
+                              .map((doc) => ViewItem(doc))
+                              .toList());
+                    } else {
+                      return SizedBox();
+                    }
+                  }),
+            ],
+          ),
+        ));
+  }
+}
