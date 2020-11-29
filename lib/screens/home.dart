e501738be3ab14656823c96787333b98440c498c
박석원 <rty3394@naver.com>
2020-11-29 22:28:03 +0900

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,9 +4,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'edit.dart';
 
 class checkTime extends StatefulWidget {
-  checkTime({Key key, this.title}) : super(key: key);
+  checkTime({Key key, this.month, this.day}) : super(key: key);
 
-  final String title;
+  final int month;
+  final int day;
 
   @override
   _checkTimeState createState() => _checkTimeState();
@@ -17,56 +18,72 @@ class _checkTimeState extends State<checkTime> {
   String ident;
   int temp1 = 0;
   int temp2 = 0;
-  int avgCreateTime = 0;
-  int avgPrepTime = 0;
-  int Count = 0;
+  int useTime = 0;
+  int readyTime = 0;
+  int count = 0;
+
 
   @override
   Card buildItem(DocumentSnapshot doc) {
+    int c = 0;
     final id = doc.data();
-    //관련 데이터 베이스 밀어야 오류 안남 원래 String으로 데이터 저장했는데 int로 저장되도록 바꿔서 생기는 오류
-    temp1 = id['createTime'];
-    temp2 = id['prepTime'];
-    avgCreateTime += temp1;
-    avgPrepTime += temp2;
-    Count += 1;
-    return Card(
-        color: Colors.white,
-        child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Table(children: [
-              TableRow(
-                children: [
-                  Text(
-                    '${id['enterTime']}  ',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+
+    Timestamp a = id['start'];
+    Timestamp b = id['end'];
+    temp1 = a.toDate()
+        .month;
+    temp2 = a.toDate()
+        .day;
+    if (b != null) {
+      print(a);
+      print(b);
+      c = b.seconds - a.seconds;
+      print(c / 60);
+    }
+
+    useTime += c;
+    readyTime += id['wait'];
+
+    count++;
+
+      return Card(
+          color: Colors.white,
+          child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Table(children: [
+                TableRow(
+                  children: [
+                    Text(
+                      '${a.toDate()}  ',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                  Text('${id['price']}￦  ',
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      textAlign: TextAlign.right),
-                  Text('${id['createTime']}분',
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      textAlign: TextAlign.right),
-                  Text('${id['prepTime']}분',
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      textAlign: TextAlign.right),
-                  Padding(padding: EdgeInsets.all(0.0)),
-                ],
-              ),
-            ])));
+                    Text('${id['total']}￦  ',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                        textAlign: TextAlign.right),
+                    Text('${(c / 60).toStringAsFixed(0)}분',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                        textAlign: TextAlign.right),
+                    Text('${id['wait']}분',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                        textAlign: TextAlign.right),
+                    Padding(padding: EdgeInsets.all(0.0)),
+                  ],
+                ),
+              ])));
+
   }
 
   Widget build(BuildContext context) {
@@ -130,13 +147,13 @@ class _checkTimeState extends State<checkTime> {
                 ),
               ),
               Text(''),
-              Text((avgCreateTime / Count).toStringAsFixed(0) + '분',
+              Text((useTime / (60*count)).toStringAsFixed(0) + '분',
                   style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                       color: Colors.black),
                   textAlign: TextAlign.right),
-              Text((avgPrepTime / Count).toStringAsFixed(0) + '분',
+              Text((readyTime / count).toStringAsFixed(0) + '분',
                   style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
